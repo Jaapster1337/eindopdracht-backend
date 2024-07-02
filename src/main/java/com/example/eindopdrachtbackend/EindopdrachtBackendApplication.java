a4c65ee218255e833b5a8755d4c0d67c112f4166
LorelessVictory <ejboutens1337@gmail.com>
2024-07-02 11:46:10 +0200

    first commit

---
+++ src/main/java/com/example/eindopdrachtbackend/EindopdrachtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eindopdrachtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindopdrachtBackendApplication.class, args);
+	}
+
+}
