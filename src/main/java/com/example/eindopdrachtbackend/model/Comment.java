6540d0694c0e013ed0ab254bd3b38bac3cbc752a
LorelessVictory <ejboutens1337@gmail.com>
2024-07-09 11:10:32 +0200

    annotaties toegevoegd en models verder uitgewerkt

--- src/main/java/com/example/eindopdrachtbackend/model/Comment.java
+++ src/main/java/com/example/eindopdrachtbackend/model/Comment.java
@@ -1,11 +1,24 @@
 package com.example.eindopdrachtbackend.model;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+@Getter
+@Setter
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
     private String content;
     private LocalDate postDate;
     private Long amountOfLikes;
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
 }
--- src/main/java/com/example/eindopdrachtbackend/model/Genre.java
+++ src/main/java/com/example/eindopdrachtbackend/model/Genre.java
@@ -1,7 +1,20 @@
 package com.example.eindopdrachtbackend.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
+    @ManyToMany(mappedBy = "genre")
+    private List<Game> listOfGames;
 }
